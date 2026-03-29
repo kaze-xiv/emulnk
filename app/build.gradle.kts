a8b80ab130835027eae7038559f5efb5c349e185
mis0suppe <misosuppe@proton.me>
2026-03-29 03:00:40 +0200

    chore: bump version to v0.5.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.emulnk"
         minSdk = 33
         targetSdk = 35
-        versionCode = 10
-        versionName = "0.5.5"
+        versionCode = 11
+        versionName = "0.5.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
